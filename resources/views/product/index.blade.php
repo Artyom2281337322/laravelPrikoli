eb3862fce8c88bbb5ca61990cc5817bf4ab2040f
Студенческая <@rbiu.local>
2025-10-17 09:27:03 +0500

    belongtomany

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -1,24 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Products</title>
-     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
+
 <body>
     <h1>Каталог товаров</h1>
     <p>{{ $buyer -> user -> name }}</p>
     <p>{{ $buyer -> adress }}</p>
     <div class="container">
-        
+
         @foreach($categories as $category)
         <a href="{{ route('products.create') }}">Создать продукт</a>
         <h2>{{ $category -> name }}</h2>
-     
+
         @foreach($products as $product)
-         <a href="{{ route('products.show', $product -> id) }}">
-           <div class="card">
+        <a href="{{ route('products.show', $product -> id) }}">
+            <div class="card">
                 <p>{{ $product -> name }}</p>
                 <p> {{ $product -> description }} </p>
                 <p>Цена: {{ $product -> price}} &#8381 </p>
@@ -28,11 +30,12 @@
                     @method('delete')
                     <button type="submit"> X </button>
                 </form>
-           </div>
-           </a>
-@endforeach
+            </div>
+        </a>
         @endforeach
-        
+        @endforeach
+
     </div>
 </body>
+
 </html>
